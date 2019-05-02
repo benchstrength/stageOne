3f50c9fbf2ec5377329fbb867aefbd0f2ca1c778
shramp <seamusmurchadh@gmail.com>
2019-05-02 11:43:33 -0400

    components, material ui animation and theme added (can be changed, went with default), changed config and existing components to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { SurveyComponent } from './survey/survey.component';
+// Material Imports
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, GestureConfig} from '@angular/material';
+import { PolarGraphComponent } from './polar-graph/polar-graph.component';
+import { SearchComponent } from './search/search.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AdminDashboardComponent,
+    UserDashboardComponent,
+    SurveyComponent,
+    PolarGraphComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule
   ],
-  providers: [],
+  providers: [{ provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
---
+++ src/app/polar-graph/polar-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-polar-graph',
+  templateUrl: './polar-graph.component.html',
+  styleUrls: ['./polar-graph.component.css']
+})
+export class PolarGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
