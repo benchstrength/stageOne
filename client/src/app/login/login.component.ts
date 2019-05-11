b59e24376cd3f668d2f5ed5b771e00231a54cfc7
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-05-11 12:21:31 -0400

    getting up to date with porxy etc

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient } from "@angular/common/http"
+// import { ApiCallsService } from '../apiCalls/api-calls.service';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +12,20 @@ export class LoginComponent implements OnInit {
   pageTitle: string = "Login Page";
   title: string = "Bench Strength";
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService,
+              public http: HttpClient) { }
 
   ngOnInit() {
   }
+
+public sendData() {
+  console.log("Send Data!")
+  this.http.post("/api/semi-private", {
+    message: "Sending data from Angular!"
+  }).toPromise()
+  .then(data => {debugger})
+  .catch(err => console.log(err));
+
+};
 
 }
