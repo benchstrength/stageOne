280c7bf16f1a23d8a16f88c7653db73d1979a171
Ash Mohney <44374838+ashmohney@users.noreply.github.com>
2019-05-16 15:41:12 -0400

    user skills input commit

--- client/src/app/app-routing/app-routes.ts
+++ client/src/app/app-routing/app-routes.ts
@@ -1,11 +1,12 @@
-import { GraphComponent } from '../graph/graph.component';
+import { AdminGraphComponent } from '../admin/components/admin-graph/admin-graph.component';
+// import { FiltersComponent} from '../admin/components/filters/filters.component';
 import { UserComponent } from '../user/user.component';
 import { LoginComponent } from '../login/login.component';
 import { AdminComponent } from '../admin/admin.component'
 
 export const Routes = [
     {path: '', component: UserComponent},
-    {path: 'home', component: GraphComponent},
+    {path: 'home', component: AdminGraphComponent},
     {path: 'callback', component: UserComponent},
     {path: 'logout', component: LoginComponent},
     {path: 'login', component: LoginComponent},
