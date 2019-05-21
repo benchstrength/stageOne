ea9eb06cc8c873a2c3bae6da6263ce8c795e54fc
tommaroney <jasonbeisiegel@gmail.com>
2019-05-21 11:21:58 -0400

    Chart is displaying all relevant info

--- client/src/app/admin/components/graphs/admin-graph.component.spec.ts
+++ client/src/app/admin/components/graphs/admin-graph.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UsersGraphComponent } from './users-graph.component';
+import { AdminGraphComponent } from './admin-graph.component';
 
-describe('UsersGraphComponent', () => {
-  let component: UsersGraphComponent;
-  let fixture: ComponentFixture<UsersGraphComponent>;
+describe('AdminGraphComponent', () => {
+  let component: AdminGraphComponent;
+  let fixture: ComponentFixture<AdminGraphComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersGraphComponent ]
+      declarations: [ AdminGraphComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UsersGraphComponent);
+    fixture = TestBed.createComponent(AdminGraphComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
