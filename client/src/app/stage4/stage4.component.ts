f2212b4f8dee0d334b42aeafc30bee90d8f7dd49
shramp <seamusmurchadh@gmail.com>
2019-05-31 10:55:24 -0400

    before pull

--- client/src/app/stage4/stage4.component.ts
+++ client/src/app/stage4/stage4.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-stage4',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Stage4Component implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
+
+  showSkills = false;
+
+  @Input() adminData: {
+    name:string, 
+    email:string, 
+    picture:string
+  } = {
+    name: "",
+    email: "",
+    picture: ""
+  };
+
+ userInfo = {
+   name: "",
+   email: "",
+   picture: ""
+ };
 
   ngOnInit() {
+    
+    // load user data if it exists
+    console.log(this.userInfo)
+  if (sessionStorage.getItem('userEmail')) {
+    this.userInfo = {
+      name: sessionStorage.getItem('userName'),
+      email: sessionStorage.getItem('userEmail'),
+      picture: sessionStorage.getItem('userPicture')
+    }
+  }
+  
+}
+
+  showForm() {
+    this.showSkills = !this.showSkills;
   }
 
+  theme() {
+    var element = document.getElementById("myDIV");
+    element.classList.toggle("mystyle");
+  }
 }
